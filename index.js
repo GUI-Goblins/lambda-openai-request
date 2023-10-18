e89ec9da59490a62409e143242fac88b08eec8f6
joshuashea <joshshea44@gmail.com>
2023-10-18 14:12:27 -0700

    testing GET funtion

---
+++ index.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const dynamoose = require('dynamoose');
+const AWS = require('aws-sdk');
+
+const userSchema = new dynamoose.Schema({
+  id: Number,
+  name: String,
+  age: Number,
+  race: {
+    type: String,
+    enum: ['human', 'elf', 'dwarf', 'gnome'],
+  },
+  class: {
+    type: String,
+    enum: ['fighter', 'rogue', 'sorcerer', 'cleric'],
+  },
+});
+
+const User = dynamoose.model('midterm-users', userSchema);
+
+exports.handler = async (event) => {
+  const userId = event.pathParameters.id;
+
+  try {
+    const user = await User.get({ userId });
+
+    if (user) {
+      // const lambda = new AWS.Lambda();
+
+      // const params = {
+      //   FunctionName: 'YourSecondLambdaFunctionName',
+      //   InvocationType: 'Event',
+      //   Payload: JSON.stringify({ user }),
+      // };
+
+      // const response = await lambda.invoke(params).promise();
+
+      if (response.FunctionError) {
+        throw new Error(`Lambda invocation error: ${response.FunctionError}`);
+      }
+
+      // const result = JSON.parse(response.Payload);
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify(user),
+      };
+    } else {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ message: 'User not found' }),
+      };
+    }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
